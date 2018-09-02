c87e3b3f971afe83d3ec3ea245bbb5b9e210a7c5
asgeirogm <asgeirogm@gmail.com>
2018-09-02 12:15:09 +0200

    Adding translation for question and answer in wolfram.py if speech and/or response language is not english

--- athena/modules/active/wolfram.py
+++ athena/modules/active/wolfram.py
@@ -10,23 +10,36 @@ Usage Examples:
 """
 
 import wolframalpha
+from googletrans import Translator
 
 from athena.classes.module import Module
 from athena.classes.task import ActiveTask
-from athena import settings
+from athena import log, settings
 
 wolfram_client = wolframalpha.Client(settings.WOLFRAM_KEY)
 
-
 class AnswerTask(ActiveTask):
+
+    translator = Translator()
 
     def match(self, text):
         return True
 
     def action(self, text):
+        speech_language = settings.LANG_4CODE[:2].lower()
+        if speech_language != 'en':
+            log.info('\n~ Translating to english\n')
+            translation = self.translator.translate(text, src=speech_language, dest='en')
+            text = translation.text
         try:
             query = wolfram_client.query(text)
-            self.speak(next(query.results).text)
+            response_language = settings.LANG_CODE.lower()
+            answer = next(query.results).text
+            if speech_language != 'en':
+                log.info('\n~ Translating to response language {}\n'.format(response_language))
+                translation = self.translator.translate(answer, src='en', dest=response_language)
+                answer = translation.text
+            self.speak(answer)
         except:
             self.speak(settings.NO_MODULES)
 
